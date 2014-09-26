4fcd87ea628c5732920794a2cccb82afa39c95ba
Johan <johan.f.ljungberg@gmail.com>
2014-09-26 23:32:17 +0200

    STUCT

---
+++ core/src/main/java/org/gaem/core/model/Entity.java
@@ -0,0 +1,10 @@
+package org.gaem.core.model;
+
+/**
+ * Created by Johan on 2014-09-26.
+ */
+public abstract class Entity {
+
+    private int x, y;
+
+}
---
+++ core/src/main/java/org/gaem/core/model/Player.java
@@ -0,0 +1,10 @@
+package org.gaem.core.model;
+
+/**
+ * Created by Johan on 2014-09-26.
+ */
+public class Player extends Mobile {
+
+
+
+}
