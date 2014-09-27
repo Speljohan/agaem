101a5561eed32b34d1865a762bd508d084743302
Giovanni Domenico Lombardi <SpookyButter@gmail.com>
2014-09-27 02:17:54 +0200

    Alta tilemap. Copiado de ejemplos.

--- core/src/main/java/org/gaem/core/AGame.java
+++ core/src/main/java/org/gaem/core/AGame.java
@@ -9,6 +9,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 import org.gaem.core.model.Player;
@@ -21,6 +25,11 @@ public class AGame implements ApplicationListener {
     private OrthographicCamera camera;
     private Player player;
 
+    Texture img;
+    TiledMap tiledMap;
+   // OrthographicCamera camera;
+    TiledMapRenderer tiledMapRenderer;
+
 	@Override
 	public void create () {
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -29,6 +38,9 @@ public class AGame implements ApplicationListener {
         ASSETS = new AssetManager();
         loadAssets();
         batch = new SpriteBatch();
+
+        tiledMap = new TmxMapLoader().load("map/testmap.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
         while(!ASSETS.update());
         player = new Player();
@@ -49,15 +61,21 @@ public class AGame implements ApplicationListener {
         Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
+
+
         if (ASSETS.update()) {
 
             player.update(elapsed);
             camera.position.set(player.realX + 16, player.realY + 16, 0);
             camera.update();
+
+            tiledMapRenderer.setView(camera);
+            tiledMapRenderer.render();
+
             batch.setProjectionMatrix(camera.combined);
             batch.enableBlending();
             batch.begin();
-            batch.draw(ASSETS.get("sprites/libgdx-logo.png", Texture.class), 0, 0);
+          //  batch.draw(ASSETS.get("sprites/libgdx-logo.png", Texture.class), 0, 0);
             batch.draw(player.sprite, player.realX, player.realY);
             batch.end();
         }
